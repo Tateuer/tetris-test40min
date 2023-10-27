669a718582a02a115b8cab7cf69fbbfdcd07fd83
Tateuer <tateuer@gmail.com>
2023-10-27 18:22:39 -0300

    change color

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ function draw() {
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value) {
-        context.fillStyle = "yellow";
+        context.fillStyle = "red";
         context.fillRect(x + piece.position.x, y + piece.position.y, 1, 1);
       }
     });
