ac1a8170fb6ce17f49e359a403d108fcb926e9c2
Tateuer <tateuer@gmail.com>
2023-10-30 18:22:05 -0300

    blue cube

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ function draw() {
   board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        context.fillStyle = "orange";
+        context.fillStyle = "blue";
         context.fillRect(x, y, 1, 1);
       }
     });
